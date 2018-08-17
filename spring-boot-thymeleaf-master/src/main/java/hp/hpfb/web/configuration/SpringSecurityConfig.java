ab0c2b2c2d0db340cafa9430dd39457ce9b72f0b
IanYangCa <IanYang.ca@gmail.com>
2018-08-17 09:31:31 -0400

    Remove unused imports

--- spring-boot-thymeleaf-master/src/main/java/hp/hpfb/web/configuration/SpringSecurityConfig.java
+++ spring-boot-thymeleaf-master/src/main/java/hp/hpfb/web/configuration/SpringSecurityConfig.java
@@ -4,20 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 import hp.hpfb.web.handler.CdnAccessDeniedHandler;
 import hp.hpfb.web.handler.CdnAuthenticationFailureHandler;
 import hp.hpfb.web.handler.CdnSuccessHandler;
+import hp.hpfb.web.handler.RestAuthenticationEntryPoint;
 import hp.hpfb.web.service.utils.CdnPasswordProvider;
 
 @Configuration
-//@EnableGlobalAuthentication
-//@EnableWebSecurity
 @EnableWebSecurity(debug = false)
 @Order
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -30,19 +27,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	private CdnAuthenticationFailureHandler cdnFailureHandler;
     @Autowired
 	private CdnPasswordProvider cdnPasswordProvider;
-
-//    @Override
-//	public void configure(WebSecurity web) throws Exception {
-//    	web.ignoring().antMatchers("/css/**").antMatchers("/images/**").antMatchers("/**/favicon.ico")
-//    	.antMatchers("/error/**").antMatchers("/error/**");
-//	}
+    @Autowired
+	private RestAuthenticationEntryPoint unauthorizedHandler;
 
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable()
                 .authorizeRequests()
-					.antMatchers("/*.gif","/", "/login", "/leftMenu", "/validateXML", "/j_spring_security_check", "/downloadXML", "/js/**").permitAll()
+					.antMatchers("/*.gif","/", "/login", "/leftMenu", "/validateXML", "/j_spring_security_check", "/downloadXML", "/js/**","/css/**").permitAll()
 					.antMatchers("/admin/**").hasAnyRole("ADMIN")
 					.anyRequest().authenticated()
                 .and()
@@ -58,7 +51,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
 					.permitAll()
 					.and()
-                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler).authenticationEntryPoint(unauthorizedHandler);
     }
 
     // create two users, admin and user
